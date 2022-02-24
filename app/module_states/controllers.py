563947984f9786a63c98cad8ab11160ebd39cd82
angelrene45 <ar.herrera0@gmail.com>
2022-02-23 19:27:15 -0600

    Gnereamos pdf de repuesta

--- app/module_states/controllers.py
+++ app/module_states/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
 
 from app.module_states.models import State
 from app.module_states.functions import generate_pdf
@@ -8,6 +8,9 @@ states = Blueprint('states', __name__, url_prefix='/states')
 
 @states.route("/", methods=['GET'])
 def home():
+    """
+        Method return json from all states in database
+    """
     # Get all states from database
     results = db.session.query(State).all()
     results_dict = { obj.id : { 'id': obj.id, 'name': obj.name, 'geojson': obj.geojson }  for obj in results  }
@@ -15,6 +18,9 @@ def home():
 
 @states.route("/<id_state>", methods=['GET'])
 def get_state( id_state ):
+    """
+        Method return json from state request
+    """
     # Get one record
     obj = db.session.query(State).get( id_state )
     if not obj: return f'State not found with id {id_state} ', 400
@@ -23,7 +29,13 @@ def get_state( id_state ):
 
 @states.route("/pdf/<id_state>", methods=['GET'])
 def get_pdf( id_state ):
+    """
+        Method return PDF file from state 
+    """
     state_object = db.session.query(State).get( id_state )
     if not state_object: return f'State not found with id {id_state} ', 400
     pdf = generate_pdf( state_object )
-    return 'generando pdf...'
+    response = make_response( pdf )
+    response.headers['Content-Disposition'] = f'attachment; filename="{state_object.name}.pdf"'
+    response.headers['Content-Type'] = 'application/pdf'
+    return response
--- app/module_states/functions.py
+++ app/module_states/functions.py
@@ -1,12 +1,29 @@
-
-from os import stat_result
-
+import json
+from fpdf import FPDF
 
 def generate_pdf( state_object ):
     id_state = state_object.id
     name = state_object.name
     geojson = state_object.geojson
 
-    print( id_state, name )
+    data_geojson = json.loads(geojson)
+    type_geom  = data_geojson.get('type')
 
-    pass
+    # variable pdf
+    pdf = FPDF('P', 'mm', 'Letter')
+    # Add a page
+    pdf.add_page()
+    # set style and size of font 
+    pdf.set_font("Arial",  'B', size = 15)
+    # create a cell
+    pdf.cell(0, 10, txt = name, 
+            ln = 1, align = 'C')
+    pdf.cell(0, 10, txt = f'ID: {id_state}', border = 0, ln = 1, 
+          align = '', fill = False, link = '')
+    pdf.cell(0, 10, txt = f'Type Geometry: {type_geom}', border = 0, ln = 1, 
+          align = '', fill = False, link = '')
+
+    pdf.multi_cell(w=0, h=10, txt='', border = 1, 
+                align= 'J', fill = False)
+    
+    return pdf.output(dest="S", name=name).encode('latin-1') # generate pdf in memory
