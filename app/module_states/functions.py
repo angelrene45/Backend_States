d8bc24df5dcbc6b16354775713eb8073233448cd
angelrene45 <ar.herrera0@gmail.com>
2022-02-24 11:27:17 -0600

    Refactor functions code

--- app/module_states/functions.py
+++ app/module_states/functions.py
@@ -9,14 +9,17 @@ from app.module_states.pdf_state import PDF_STATE
 http = urllib3.PoolManager()
 
 def generate_pdf( state_object ):
+    """
+        Method that generate PDF from state
+    """
     id_state = state_object.id
     name = state_object.name
     geojson = state_object.geojson
 
-    data_geojson = json.loads(geojson)
-    type_geom  = data_geojson.get('type')
+    data_geojson = json.loads(geojson) # convert string to diccionary
+    type_geom  = data_geojson.get('type') # get type of geometry 
 
-    pdf = PDF_STATE('P', 'mm', 'Letter')
+    pdf = PDF_STATE('P', 'mm', 'Letter') # crate Object from PDF
     pdf.title_header = name
     pdf.set_title(name)
     pdf.set_author('angel.calzada')
@@ -24,7 +27,7 @@ def generate_pdf( state_object ):
     pdf.print_attribute(f'ID: {id_state}')
     pdf.print_attribute(f'Type Geometry: {type_geom}')
 
-    path_image = request_image_mapbox( id_state, data_geojson )
+    path_image = request_image_mapbox( id_state, data_geojson ) # Call API mapbox to create image from GeoJSON
     if not path_image: return None
     else: pdf.setImageGeoJSON( path_image )
     
@@ -32,8 +35,14 @@ def generate_pdf( state_object ):
 
 
 def request_image_mapbox( id_state, data_geojson ):
+    """
+        Method that do request to API mapbox 
+        Create and image from GeoJSON
+    """
+    # path where image save
     path_image = os.path.join( app.config['STATIC_FOLDER'], 'states', f'{id_state}.png' )
     
+    # geojson request from mapbox API
     geojson =  {
             "type": "FeatureCollection",
             "features": [
@@ -42,20 +51,29 @@ def request_image_mapbox( id_state, data_geojson ):
                     "properties": {
                         "stroke": "#000000",
                         "fill": "#005776",
-                        "fill-opacity": 1
+                        "fill-opacity": 0.5
                     },
                     "geometry": data_geojson
                 }
             ]
-        }
+    }
+
+    # Convert diccionary to url encoded
     geojson = json.dumps(geojson).replace(" ", "") # convert diccionary in string and replace spaces
     geojson = urllib.parse.quote( geojson ) # convert string json in url encoded
-    token_mapbox = os.getenv('MAPBOX_TOKEN') # get token from env 
+    # Get mapbox token for request
+    token_mapbox = os.getenv('MAPBOX_TOKEN')
+    # Full url from api request
     api_request = f"""https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson({geojson})/auto/630x360?access_token={token_mapbox}"""
+    # Do request 
     bytes_image = make_request_api( api_request )
+    # Validate response 
     if not bytes_image: return False
+    # save image in static files 
     path_image = save_image(path_image, bytes_image)
+    # Validate exist path_image
     if not path_image: return False
+
     return path_image 
 
 def make_request_api( api_request ):
